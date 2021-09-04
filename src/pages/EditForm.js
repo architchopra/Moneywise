3120a2668ac28f2876e36ebcbce30479e7c3dd2b
architchopra <57140059+architchopra@users.noreply.github.com>
2021-09-04 23:05:00 +0530

    form final

--- src/pages/EditForm.js
+++ src/pages/EditForm.js
@@ -1,39 +1,43 @@
 import React from "react";
-import { Container, makeStyles, Card } from "@material-ui/core";
+import { Container, Card, FormControl, MenuItem } from "@material-ui/core";
+import { makeStyles } from '@material-ui/styles';
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import { ThemeProvider } from "@material-ui/core";
 import { createTheme } from "@material-ui/core/styles";
 import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
 import { useState } from "react";
-
+import "../index.css"
 import axios from "axios";
+import InputLabel from '@material-ui/core/InputLabel';
+import api from "../api"
+import Select from '@material-ui/core/Select';
 const useStyles = makeStyles(() => ({
   root: {
-    display: "flex",
+   
     marginTop: "7rem",
     flexDirection: "column",
     marginBottom: "4rem",
   },
   heading: {
     fontSize: "2rem",
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: "left",
+    alignItems: "left",
   },
   form: {
     width: "100%",
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: "left",
+    alignItems: "left",
   },
   text: {
-    marginLeft: "0.5rem",
+    marginLeft: "0.1rem",
   },
   field: {
     marginTop: "2rem",
     marginBottom: "2rem",
   },
   btn: {
-    marginLeft: "42%",
+    marginLeft: "2%",
     marginBottom: "1rem",
   },
 }));
@@ -56,31 +60,37 @@ const theme = createTheme({
 const Form = () => {
   const classes = useStyles();
   // create state variables for each input
-  const [emailValue, setEmailValue] = useState("");
-  const [feedbackValue, setFeedbackValue] = useState("");
+  const [expense, setExpense] = useState("");
+  const [cost, setCost] = useState("");
   const [textValue, setTextValue] = useState("");
   // const [,feedbackValue, setTextValue] = useState("");
 
-  const onEmailChange = (e) => setEmailValue(e.target.value);
-  const onFeedbackChange = (e) => setFeedbackValue(e.target.value);
+  const onExpenseChange = (e) => setExpense(e.target.value);
+  const onCostChange = (e) => setCost(e.target.value);
  
   const handleReset = () => setTextValue("");
 
   async function fetchData() {
-    const formData = new FormData();
-
-    formData.append("EMAIL", emailValue);
-    formData.append('MESSAGE', feedbackValue);
+    const type=expense;
+    const config ={
+      headers:{
+        'Content-Type': 'application/json',
+        'token': JSON.parse(localStorage.getItem("user")).user.token,
+      }
+    }
+    const {data} = await api.post('/api/private/expenses/add',{type,cost},config);
+    // formData.append("type", expense);
+    // formData.append('cost', cost);
 
    // const state = { EMAIL:emailValue,MESSAGE:feedbackValue}
-    console.log(formData);
+    // console.log(formData);
     //  const dispatch = useDispatch();
     // dispatch(postUserData());
 
-    const { data } = await axios.post(
-      'https://zxtsbd2v6e.execute-api.us-east-1.amazonaws.com/prod/support',
-      formData
-   )
+  //   const { data } = await axios.post(
+  //     'https://zxtsbd2v6e.execute-api.us-east-1.amazonaws.com/prod/support',
+  //     formData
+  //  )
    // setFetchedData(data)
   }
 
@@ -97,36 +107,36 @@ const Form = () => {
           <Card className={classes.root}>
             <Container maxWidth="sm">
               <div className={classes.form}>
-                <H3 className="formheading">Contact us | Monk</H3>
-
-                <TextField
-                  label="Enter your email:"
-                  onChange={onEmailChange}
-                  InputLabelProps={{
-                    shrink: true,
-                  }}
-                  variant="outlined"
-                  type="email"
-                  value={emailValue}
-                  required
-                  fullWidth
-                  placeholder="your@email.com"
-                />
+                <h3 className="formheading">Contact us | Moneywise</h3>
+                <FormControl className={classes.formControl}>
+        <InputLabel id="demo-simple-select-label">TYPE OF EXPENSE</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={expense}
+          onChange={onExpenseChange}
+          autoWidth
+        >
+          <MenuItem value="household">Household Expense</MenuItem>
+          <MenuItem value="shopping">Shopping Expense</MenuItem>
+          <MenuItem value="investment">Investment and Loan Expense</MenuItem>
+          <MenuItem value="misc">Miscellenious Expense</MenuItem>
+        </Select>
+      </FormControl>
+               
 
                 <TextField
                   label=" How can we assist you?"
-                  onChange={onFeedbackChange}
+                  onChange={onCostChange}
                   InputLabelProps={{
                     shrink: true,
                   }}
                   className={classes.field}
                   variant="outlined"
-                  value={feedbackValue}
+                  value={cost}
                   required
                   fullWidth
-                  multiline
-                  rows={4}
-
+                  
                   // value={password}
                   // onChange={e => setPassword(e.target.value)}
                 />
