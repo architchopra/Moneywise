2a1e5f185512d17b9c2cc1b099c0bc93785a2942
shivanshgupta340 <shivanshgupta34@gmail.com>
2021-09-04 17:07:01 +0530

    loaded template

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import 'react-perfect-scrollbar/dist/css/styles.css';
+import { useRoutes } from 'react-router-dom';
+import { ThemeProvider, StyledEngineProvider } from '@material-ui/core';
+import GlobalStyles from './components/GlobalStyles';
+import theme from './theme';
+import routes from './routes';
 
-import './App.css';
+const App = () => {
+  const content = useRoutes(routes);
 
-function App() {
   return (
-    <div className="App">
-      <h1>MONEYWISE</h1>
-    </div>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        {content}
+      </ThemeProvider>
+    </StyledEngineProvider>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-/* eslint-disable import/default */
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
-import React from 'react';
-import { render } from 'react-dom';
-import { Router, browserHistory } from 'react-router';
-import routes from './routes';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-require('./favicon.ico');
-import './styles.scss';
-import 'font-awesome/css/font-awesome.css';
-import 'flexboxgrid/css/flexboxgrid.css';
-
-injectTapEventPlugin();
-
-render(
-    <Router routes={routes} history={browserHistory} />, document.getElementById('app')
+ReactDOM.render(
+  (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  ),
+  document.getElementById('root')
 );
